9cd607e7b63921b9be1b2188f32e472d4fd6b6e1
Jason Walker <ungood@onetrue.name>
2012-07-27 14:10:46 -0600

    Found that M(2x) = M(x) in risky moons

---
+++ Euler/RiskyMoon/Tests/RiskCalculatorTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Puzzles.Common.Spatial;
+using Puzzles.Euler.RiskyMoon.Lattice;
+using Puzzles.Euler.RiskyMoon.Risk;
+
+namespace Puzzles.Euler.RiskyMoon.Tests
+{
+    [TestFixture]
+    public class RiskCalculatorTests
+    {
+        public IEnumerable<TestCaseData> GetImplementations()
+        {
+            return new[]
+            {
+                new TestCaseData(new AStarRiskCalculator())
+            };
+        } 
+
+        [Test]
+        [TestCaseSource("GetImplementations")]
+        public void UnitCircleTest(IRiskCalculator calculator)
+        {
+            var unitCircle = new []
+            {
+                new CartesianCoordinates(0, 0, 1),
+                new CartesianCoordinates(0, 1, 0),
+                new CartesianCoordinates(1, 0, 0),
+                new CartesianCoordinates(0, 0, -1),
+                new CartesianCoordinates(0, -1, 0),
+                new CartesianCoordinates(-1, 0, 0)
+            };
+
+            var stations = unitCircle.Select(coord => new Station(coord));
+
+            var actual = calculator.CalculateRisk(stations, 1);
+
+            Assert.AreEqual(0.5, actual);
+        }
+
+        [Test]
+        [TestCaseSource("GetImplementations")]
+        public void GivenExampleTest(IRiskCalculator riskCalculator)
+        {
+            var calculator = new ParallelNaiveSphereLatticeCalculator();
+            var stations = calculator.FindLatticePoints(7).Select(x => new Station(x));
+            var actual = riskCalculator.CalculateRisk(stations, 7);
+
+            Assert.AreEqual(0.1784943998, Math.Round(actual, 10));
+        }
+
+        [Test]
+        public void FirstThirtyAsCsv()
+        {
+            var latticeCalculator = new SliceSphereLatticeCalculator(new TurtleCircleLatticeCalculator());
+            var riskCalculator = new AStarRiskCalculator();
+
+            var results = Enumerable.Range(1, 30).AsParallel()
+                .Select(radius =>
+                {
+                    var stations = latticeCalculator.FindLatticePoints(radius).Select(coord => new Station(coord));
+                    return new Tuple<int, double>(radius, riskCalculator.CalculateRisk(stations, radius));
+                })
+                .OrderBy(grp => grp.Item1);
+                
+
+            foreach (var result in results)
+            {
+                Console.WriteLine("{0}\t{1}", result.Item1, result.Item2);
+            }
+        }
+
+        /// <summary>
+        /// Interesting to note that a moon with radius 2n has the same minimal risk as a moon with radius n.
+        /// M(2r) = M(r)
+        /// </summary>
+        // 0.06924307373552: 27
+        // 0.07394084651690: 21
+        // 0.10140133687823: 25
+        // 0.10296540470811: 19
+        // 0.10599928132709: 23
+        // 0.10714794796212: 15, 30
+        // 0.12091813090701: 29
+        // 0.12169805976751: 9, 18
+        // 0.13403020289275: 17
+        // 0.16087118102414: 11, 22
+        // 0.17849439975514: 28, 7, 14
+        // 0.18175660153131: 26, 13
+        // 0.18414647913495: 10, 20, 5
+        // 0.23604566644613: 3, 24, 6, 12
+        // 0.50000000000000: 2, 16, 8, 4, 1
+        [Test]
+        public void FirstThirtyGroupedByRisk()
+        {
+            var latticeCalculator = new SliceSphereLatticeCalculator(new TurtleCircleLatticeCalculator());
+            var riskCalculator = new AStarRiskCalculator();
+
+            var groups = Enumerable.Range(1, 30).AsParallel()
+                .Select(radius =>
+                {
+                    var stations = latticeCalculator.FindLatticePoints(radius).Select(coord => new Station(coord));
+                    return new Tuple<int, int, double>(radius, stations.Count(), riskCalculator.CalculateRisk(stations, radius));
+                })
+                .GroupBy(tuple => tuple.Item3)
+                .OrderBy(grp => grp.Key);
+
+            foreach(var grp in groups)
+            {
+                Console.Write(grp.Key + ":");
+                foreach(var value in grp.OrderBy(tuple => tuple.Item1))
+                {
+                    Console.Write(" {0}({1})", value.Item1, value.Item2);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+        [Test]
+        public void FindSolutions()
+        {
+            var m15 = 0.10714794796212; // ~83 (1.463)
+            var m5 = 0.18414647913495;
+            var m3 = 0.23604566644613;
+        }
+
+    }
+}
